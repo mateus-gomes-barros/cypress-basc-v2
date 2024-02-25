0eb3437cf4a2188fa4444ebd3b89704854b74855
mateus-gomes-barros <mateusgomesbarros@hotmail.com>
2024-02-25 19:21:48 -0300

    firstTestFinished

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -4,78 +4,99 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
-/// <reference types="Cypress"/>
+/// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function(){
-    beforeEach(function () {
-        cy.visit('./src/index.html') 
-    })
-    it('verifica o título da aplicação', function(){
-      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-    })
-    it('preenche os campos obrigatórios e envia o formulário', function(){
-        const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste'
-        cy.get('#firstName').type('Mateus')
-        cy.get('#lastName').type('Gomes')
-        cy.get('#email').type('mateus.gomes@vturb.com')
-        cy.get('#open-text-area').type(longText, {delay:0})
-        cy.get('button[type="submit"]').click()
+describe("VTurb", function () {
+  before(() => {
+    // Configura o Cypress para preservar cookies entre os testes
+    Cypress.Cookies.defaults({
+      preserve: ["jwtToken"] // Substitua "jwtToken" pelo nome do seu cookie JWT
+    });
+  });
 
-        cy.get('.success').should('be.visible')
-    })
-    it('exibe mensagem de erro ao submeter o formulário com um email de formatação inválida', function(){
-        cy.get('#firstName').type('Mateus')
-        cy.get('#lastName').type('Gomes')
-        cy.get('#email').type('mateus.gomes@vturb,com')
-        cy.get('#open-text-area').type('Teste')
-        cy.get('button[type="submit"]').click()
+  beforeEach(() => {
+    // Visita apenas a página de login
+    cy.visit("https://login.vturb.com");
 
-        cy.get('.error').should('be.visible')
-      })
-      it('Verificação se contém apenas números', function(){
-        cy.get('#phone')
-        .type('abcdefghij')
-        .should('have.number', '')
-      })
-      it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
-        cy.get('#firstName').type('Mateus')
-        cy.get('#lastName').type('Gomes')
-        cy.get('#email').type('mateus.gomes@vturb.com')
-        cy.get('#phone-checkbox').click()
-        cy.get('#open-text-area').type('Teste')
-        cy.get('button[type="submit"]').click()
+    // Certifique-se de que a página não será recarregada
+    cy.server();
+    cy.route("POST", "**/api/login").as("loginRequest");
+  });
 
-        cy.get('.error').should('be.visible')
-      })
+  it("Autenticação e Uso de Token JWT", function () {
+    // Realiza a autenticação na aplicação
+    cy.get('input[name="email"]').type("contatoarqalexsoares@gmail.com");
+    cy.get('input[name="password"]').type("Alex3d0809@");
+    cy.contains("span", "Entrar").click();
 
-      it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
-        cy.get('#firstName')
-        .type('Mateus')
-        .should('have.value', 'Mateus')
-        .clear()
-        .should('have.value', '')
-        cy.get('#lastName')
-        .type('Gomes')
-        .should('have.value', 'Gomes')
-        .clear()
-        .should('have.value', '')
-        cy.get('#email')
-        .type('mateusgomes@hotmail.com')
-        .should('have.value', 'mateusgomes@hotmail.com')
-        .clear()
-        .should('have.value', '')
-        cy.get('#phone')
-        .type('999836351')
-        .should('have.value', '999836351')
-        .clear()
-        .should('have.value', '')
-        
-      })
-      it('envia formulário com sucesso usando comando customizado', function() {    
-       
-        cy.fillMandatoryFieldsAndSubmit()
- 
-        cy.get('.success').should('be.visible') 
-    })
- 
-})
+    // Aguarde a resposta da solicitação de login
+    cy.wait("@loginRequest").then((xhr) => {
+      // Obtenha o token JWT da resposta da solicitação de login
+      const token = xhr.response.body.token;
+
+      // Defina manualmente o cookie JWT
+      cy.setCookie("access_token", tokenJWT);
+    });
+
+    // Visite a página do painel após o login
+    cy.visit("https://login.vturb.com/dashboard");
+
+    // Agora você pode prosseguir com os testes que requerem o token JWT
+    // ...
+  });
+});
+
+// it('exibe mensagem de erro ao submeter o formulário com um email de formatação inválida', function(){
+//     cy.get('#firstName').type('Mateus')
+//     cy.get('#lastName').type('Gomes')
+//     cy.get('#email').type('mateus.gomes@vturb,com')
+//     cy.get('#open-text-area').type('Teste')
+//     cy.get('button[type="submit"]').click()
+
+//     cy.get('.error').should('be.visible')
+//   })
+//   it('Verificação se contém apenas números', function(){
+//     cy.get('#phone')
+//     .type('abcdefghij')
+//     .should('have.number', '')
+//   })
+//   it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+//     cy.get('#firstName').type('Mateus')
+//     cy.get('#lastName').type('Gomes')
+//     cy.get('#email').type('mateus.gomes@vturb.com')
+//     cy.get('#phone-checkbox').click()
+//     cy.get('#open-text-area').type('Teste')
+//     cy.get('button[type="submit"]').click()
+
+//     cy.get('.error').should('be.visible')
+//   })
+
+//   it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+//     cy.get('#firstName')
+//     .type('Mateus')
+//     .should('have.value', 'Mateus')
+//     .clear()
+//     .should('have.value', '')
+//     cy.get('#lastName')
+//     .type('Gomes')
+//     .should('have.value', 'Gomes')
+//     .clear()
+//     .should('have.value', '')
+//     cy.get('#email')
+//     .type('mateusgomes@hotmail.com')
+//     .should('have.value', 'mateusgomes@hotmail.com')
+//     .clear()
+//     .should('have.value', '')
+//     cy.get('#phone')
+//     .type('999836351')
+//     .should('have.value', '999836351')
+//     .clear()
+//     .should('have.value', '')
+
+//   })
+//   it.only('envia formulário com sucesso usando comando customizado', function() {
+
+//     cy.fillMandatoryFieldsAndSubmit()
+
+//     cy.get('.success').should('be.visible')
+// })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,29 @@
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(){
-    cy.get('#firstName').type('Mateus')
-    cy.get('#lastName').type('Gomes')
-    cy.get('#email').type('mateus.gomes@vturb.com')
-    cy.get('#open-text-area').type('Teste')
-    cy.get('button[type="submit"]').click()
-})
+import "cypress-localstorage-commands";
+
+Cypress.Commands.add("fillMandatoryFieldsAndSubmit", function () {
+  cy.request({
+    method: "POST",
+    url: "https://api.vturb.com.br/auth/login.json",
+    body: {
+      login: {
+        email: "contatoarqalexsoares@gmail.com",
+        password: "Alex3d0809@",
+      },
+    },
+  }).then((response) => {
+    let tokenJWT = response.body.token;
+    cy.setLocalStorage("access_token", tokenJWT);
+    cy.setCookie("access_token", tokenJWT);
+    cy.route({
+      method: "POST",
+      url: "https://api.vturb.com.br/auth/login.json",
+      response: { token: tokenJWT }, // Pode ajustar conforme necessário
+    });
+  });
+
+  cy.get("#firstName").type("Mateus");
+  cy.get("#lastName").type("Gomes");
+  cy.get("#email").type("mateus.gomes@vturb.com");
+  cy.get("#open-text-area").type("Teste");
+  cy.get('button[type="submit"]').click();
+});
